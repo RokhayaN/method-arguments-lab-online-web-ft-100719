3d238a2ace2488e2515031f9d6e997aeb938dcc3
RokhayaN <rokhayamuslima1@gmail.com>
2019-10-12 09:06:50 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,9 +1,7 @@
 def introduction ("rokhaya")
-  
-  puts "Hi, my name is #{rokhaya}."
+  puts "Hi, my name is #{name}."
 end
 
-def introduction ("rokhaya" , ruby)
-  
+def introduction_with_laguage ("rokhaya" , ruby)
   puts "Hi , my name is #{name} and i m a #{language} student"
 end
